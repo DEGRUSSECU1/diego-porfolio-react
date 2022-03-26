01fef4d3993fc6534b3dc01d8fa76a25bb3787ba
Diego Russo <diego25.russo@gmail.com>
2022-03-26 20:48:24 +0000

    home page done

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,8 +1,12 @@
+import { HomePage } from "./components/HomePage/HomePage";
+
+import "./components/css/App.css";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <HomePage/>
+    </>
   );
 }
 
---
+++ my-portfolio/src/components/HomePage/HomePage.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+
+export const HomePage = () => {
+  return (
+    <div className='container'>
+      {/* beginning of navbar */}
+      <nav className='nav-bar'> 
+          <div className='box'>
+           <a href='https://www.google.com'><img src='./img/spaceman.png' alt="linkedinprofile" /></a>
+           </div>
+           <div className='box'>
+           <a href='https://github.com/DEGRUSSECU1'><img src="./img/github.png" alt='githubprofile' /></a>
+           </div>
+           <div className='box'>
+           <a href='https://www.linkedin.com/in/diego-russo94/'><img src="./img/linkedin.png" alt="linkedinprofile" /></a>
+           </div>
+      </nav>
+      {/* end of navbar */} 
+            
+      {/* beginning of name container */} 
+            <div className='name-container'>
+         <h1>Hi, I'm Diego </h1>
+        <div className='star-box'>
+            <img src="./img/star.png" className='star'/>
+        </div>
+        <h2>Software Developer</h2>
+         </div> 
+       {/* beginning of name container */} 
+       
+        <div className='container-sun'>
+          
+            <img src="./img/nebula.png" className='nebula' />
+            <img src="./img/sundiego.png" className='avatar'/>
+        </div>
+       
+         <div className='rocket-container'>
+          <img src="./img/school.png" className='rocket' /> 
+          <img src="./img/rocket.png" className='rocket' /> 
+       
+          </div>
+        <div className='button'>
+           
+            <button className='btn'>
+                About me
+            </button>
+          
+        </div>
+     
+    </div>
+  )
+}
