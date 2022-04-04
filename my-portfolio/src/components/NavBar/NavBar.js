9ebe0e4465dfdd1c0c5cfcf18a25e32927651d2d
Diego Russo <diego25.russo@gmail.com>
2022-04-04 20:18:28 +0100

    test

--- my-portfolio/src/components/NavBar/NavBar.js
+++ my-portfolio/src/components/NavBar/NavBar.js
@@ -10,7 +10,7 @@ export const NavBar = () => {
    
      <h1>Welcome<br/> to my <br/> portfolio</h1>
      <a href='https://github.com/DEGRUSSECU1'><img src="./img/github.png" alt='githubprofile' className={css.box} /></a>
-     <p>github</p>
+     <p >github</p>
      <a href='https://www.linkedin.com/in/diego-russo94/'><img src="./img/linkedin.png" alt="linkedinprofile" className={css.box} /></a>
      <p>Linkedin</p>
      </nav>
